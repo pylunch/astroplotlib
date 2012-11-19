88c379509d11066b4f90465bc40d4d1668510a2a
astrocaribe <tlcommodore@gmail.com>
2012-11-19 15:29:49 -0500

    Completed simple_plot_3_python.py
    Completed the Python equivalent of Leonardo's simple_plot_3_idl.pro

---
+++ simple_plot/simple_plot_3_python.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+
+# Header
+
+"""
+Plots random data as red squares. Obtains a linear fit and overplots the fit.
+
+:Autor: Tommy Le Blanc
+
+:Organization: Space Telescope Science Intitute
+
+Example
+-------
+
+>>> python simple_plot_3_python
+"""
+
+# Import necessary modules
+import pyfits as py
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define some random data
+np.random.seed(5)
+x = np.random.standard_normal(400) * 12.
+np.random.seed(1)
+y = x + np.random.standard_normal(len(x))
+
+# Perform a line fit to the data
+line = np.polyfit(x, y, 1)
+
+# Define x-values for line fit
+x_fit = np.arange(21)
+y_fit = line[0] * x_fit + line[1]
+
+# Setup plot, size in inches
+fig, ax = plt.subplots(figsize = (4.724, 4.724))
+
+# Define axes
+ax.set_xlabel('x title')
+ax.set_ylabel('y title')
+ax.set_xlim(0, 12)
+ax.set_ylim(0, 12)
+
+# Plot data
+ax.plot(x, y, 'rs')
+ax.plot(x_fit, y_fit, 'b--')
+
+# Save figure
+fig.savefig('plot.pdf')
