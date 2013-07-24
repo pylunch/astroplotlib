a71f4699f26a64cbef6840988b9fe359e5c4860f
astrocaribe <tlcommodore@gmail.com>
2013-07-24 16:55:33 -0400

    Updated datacube scripts
    - Updated header info
    - Optimized FITS file input
    - Tweaked plot displays

--- datacubes/datacube_1_python.py
+++ datacubes/datacube_1_python.py
@@ -2,48 +2,60 @@
 
 # Header
 
-"""
-Displays a contour plot of a datacube slice
+'''
+ NAME:                datacube_1_python
 
-:Authors: Derek Hammer, Tommy Le Blanc
+ DESCRIPTION:         Displays a contour plot on a datacube slice.
 
-:Organization: Space Telescope Science Institute
+ EXECUTION COMMAND:
+                      >>> python datacube_1_python.py
+ 
+ INPUTS:              FITS datacube: ngc4151_hband.fits
+                    
+ OUTPUTS:             PDF file: python_plot.pdf
 
-Example
--------
 
->>> python datacubes_1_python
-"""
+ AUTHOR(S):           Derek HAMMER
+ 					  Tommy LE BLANC
+                      Space Telescope Science Institute, USA 
+ 
+ REVISION HISTORY:
+                      * Written by Derek HAMMER, Tommy LE BLANC, NOV 2012.
 
+                      * JUL 2013 - Optimized FITS datacube input, tweaked
+                                 plot display.
+'''
 
 # Import necessary modules
 import pyfits as pf
 import matplotlib.pyplot as plt
 
-# read datacube
-f = pf.open('ngc4151_hband.fits')
-cube = f[1].data
-header = f[1].header
-f.close()
+# Name the output file
+psname = 'python_plot.ps'
 
-# we plot slice 1067
+# Read datacube
+cube, header = pf.getdata('./data/ngc4151_hband.fits', header=True)
+
+# We plot slice 1067
 cube_slice = 1067
 image = cube[cube_slice, :, :]
 tam = image.shape
 
-# display the background image
-fig, ax = plt.subplots()
-alplot = ax.imshow(image, vmin = 0, vmax = 100)
-alplot.set_cmap('hot')
+# Display the background image
+cmap = plt.cm.get_cmap('hot')
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.imshow(image, cmap=cmap, vmin = 0, vmax = 100, interpolation='nearest')
 
-# define axes
+# Define axes
 ax.set_xlabel('x pixels')
 ax.set_ylabel('y pixels')
 ax.set_ylim([0, tam[0]-1])
 ax.set_xlim([0, tam[1]-1])
 
-# create contours
+# Create contours
 levels = [5, 10, 20, 30, 40, 50, 100, 200, 600]
-ax.contour(image, levels, colors='g')
+cplot = ax.contour(image, levels, colors='Lime')
+cplot.clabel(inline=1, fontsize=8, fmt='%2d')
 
-fig.savefig('plot.pdf')
+# Save figure
+fig.savefig(psname)
--- datacubes/datacube_2_python.py
+++ datacubes/datacube_2_python.py
@@ -2,32 +2,42 @@
 
 # Header
 
-"""
-Display the spectrum of one pixel of a datacube
+'''
+ NAME:                datacube_2_python
 
-:Author: Tommy Le Blanc
+ DESCRIPTION:         Displays the spectrum of one pixel of a datacube.
 
-:Organization: Space Telescope Science Intitute
+ EXECUTION COMMAND:
+                      >>> python datacube_2_python.py
+ 
+ INPUTS:              FITS datacube: ngc4151_hband.fits
+                    
+ OUTPUTS:             PDF file: python_plot.pdf
 
-Example
--------
 
->>> python datacubes_2_python.py
-"""
+ AUTHOR(S):			  Tommy LE BLANC
+                      Space Telescope Science Institute, USA 
+ 
+ REVISION HISTORY:
+                      * Written by Tommy LE BLANC, NOV 2012.
+
+                      * JUL 2013 - Optimized FITS datacube input, tweaked
+                                   plot display.
+'''
 
 # Import necessary modules
 import pyfits as pf
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Name output file
+psname = 'python_plot.ps'
+
 # Read datacube
 # This datacube is not continuum subtracted
-f = pf.open('ngc4151_hband.fits')
-cube = f[1].data
-hdr = f[1].header
-f.close()
+cube, hdr = pf.getdata('./data/ngc4151_hband.fits', header=True)
 
-# Calibrate in wavelength from header keywords
+# Calibrate wavelength from header keywords
 crpix3 = hdr['CRPIX3']
 cdelt3 = hdr['CDELT3']
 crval3 = hdr['CRVAL3']
@@ -37,21 +47,20 @@ tam = cube.shape
 pixel = np.arange(tam[0]) + 1.0
 lamb = crval3 + (pixel - crpix3) * cdelt3
 
-# Select pixel with coodinates [30, 30]
+# Select pixel with coodinates [30, 30] in range of datacube
 flux = cube[100:1900, 30, 30]
 lambda_microns = lamb[100:1900] / 1.e4
 
-# Setup plot, figure size in inches
-fig, ax = plt.subplots(figsize = (6.7, 2.75))
+# Setup plot
+fig, ax = plt.subplots(figsize = (12, 4))
 
-# Define axes
-ax.set_xlabel('wavelength ('+'$\mu$'+'m)')
-ax.set_ylabel('flux')
-ax.set_xlim([min(lambda_microns), max(lambda_microns)])
-ax.set_ylim([min(flux), max(flux)])
+ax.set_xlabel('wavelength ('+'$\mu$'+'m)', size=12)
+ax.set_ylabel('flux', size=12)
+ax.set_xlim([min(lambda_microns), max(lambda_microns)], plt.minorticks_on())
+ax.set_ylim([min(flux), max(flux)], plt.minorticks_on())
 
 # Plot spectrum
-ax.plot(lambda_microns, flux, 'r-', linewidth = 2)
+ax.plot(lambda_microns, flux, color='red', lw=1)
 
 # Save figure
-fig.savefig('plot.pdf')
+fig.savefig(psname, orientation='landscape')
